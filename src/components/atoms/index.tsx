37a4672406142985daf62fca5e1458070d42dab4
Lucas Santana <lucas.santana349@gmail.com>
2022-07-30 17:02:16 -0300

    Adding an export file

---
+++ src/components/atoms/index.tsx
@@ -0,0 +1,15 @@
+import { ButtonAdd, ButtonDelete } from "./Buttons/ButtonIcon";
+import { ButtonTextPrimary, ButtonTextDanger } from "./Buttons/ButtonText";
+import { Dropdown } from "./Dropdown";
+import { Hyperlink } from "./Hyperlink";
+import { Input } from "./Input";
+
+export {
+  ButtonAdd,
+  ButtonDelete,
+  ButtonTextPrimary,
+  ButtonTextDanger,
+  Dropdown,
+  Hyperlink,
+  Input,
+};
---
+++ src/components/molecules/index.tsx
@@ -0,0 +1,7 @@
+import { InvoiceForm } from "./InvoiceForm";
+import { InvoiceHeader } from "./InvoiceHeader";
+import { TableData } from "./TableData";
+import { TableHeader } from "./TableHeader";
+import { TableRow } from "./TableRow";
+
+export { InvoiceForm, InvoiceHeader, TableData, TableHeader, TableRow };
