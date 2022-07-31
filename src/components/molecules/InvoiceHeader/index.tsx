1dff63bcc6bc5c04fb7460d8932ddf68cab8b3c6
Lucas Santana <lucas.santana349@gmail.com>
2022-07-31 19:44:17 -0300

    Styling header component

--- src/components/molecules/InvoiceHeader/index.tsx
+++ src/components/molecules/InvoiceHeader/index.tsx
@@ -2,12 +2,38 @@ import React, { FC } from "react";
 
 import { Header as StyledHeader } from "./styles";
 
+import { Hyperlink, Label, InputBold } from "../../atoms";
+
 interface Props {
-  children: React.ReactNode;
+  children?: React.ReactNode;
+  image?: string;
 }
 
-const InvoiceHeader: FC<Props> = ({ children }) => {
-  return <StyledHeader>{children}</StyledHeader>;
+const InvoiceHeader: FC<Props> = ({ image, children }) => {
+  return (
+    <StyledHeader>
+      <div className="title">
+        <h3>INVOICE</h3>
+      </div>
+
+      <div className="content">
+        <div className="invoice-number">
+          <Label color="#333" htmlFor="invoice-number-input">
+            <strong>Invoice #</strong>
+          </Label>
+          <InputBold id="invoice-number-input"></InputBold>
+        </div>
+
+        <div className="invoice-img">
+          <img src={image} alt="Invoice logo" />
+          <div className="img-controllers">
+            <Hyperlink href="">Edit logo</Hyperlink>
+            <Hyperlink href="">Hide logo</Hyperlink>
+          </div>
+        </div>
+      </div>
+    </StyledHeader>
+  );
 };
 
 export { InvoiceHeader };
--- src/components/molecules/InvoiceHeader/styles.tsx
+++ src/components/molecules/InvoiceHeader/styles.tsx
@@ -1,5 +1,50 @@
 import styled from "styled-components";
 
-const Header = styled.header``;
+const Header = styled.header`
+  margin: 2rem 0;
+
+  .title {
+    box-sizing: border-box;
+    background: ${(props) => props.theme.primary};
+    color: white;
+    width: 100%;
+    text-align: center;
+    padding: 0.5rem;
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5 {
+      margin: 0;
+      font-weight: normal;
+    }
+  }
+
+  .content {
+    padding: 1rem 1rem 0 1rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+
+    .invoice-img {
+      justify-self: end;
+
+      img {
+        max-width: 300px;
+      }
+
+      .img-controllers {
+        display: flex;
+        justify-content: end;
+        gap: 0.5rem;
+
+        a {
+          text-decoration: none;
+          font-size: 18px;
+        }
+      }
+    }
+  }
+`;
 
 export { Header };
