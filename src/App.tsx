caa203de17517ccd0f69ad8f7a1eff5e26b02c90
Lucas Santana <lucas.santana349@gmail.com>
2022-07-31 19:47:13 -0300

    Adding header and footer component to webpage

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,46 @@ import "./App.css";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 
-import { ButtonAdd, ButtonDelete, Input } from "./components/atoms";
-import { InvoiceForm } from "./components/molecules";
+import {
+  ButtonAdd,
+  ButtonDelete,
+  Input,
+  ButtonTextPrimary,
+  Hyperlink,
+} from "./components/atoms";
+import {
+  InvoiceForm,
+  InvoiceHeader,
+  InvoiceFooter,
+} from "./components/molecules";
 import { Table } from "./components/organisms";
 import { Container } from "./components/Container";
 
+import metaware_logo from "./assets/metaware_logo.png";
+
 function App() {
-  const inputs = [
-    { key: "field1", value: "Mr. John Doe" },
-    { key: "field2", value: "John Doe Designs Inc." },
-    { key: "field3", value: "1 Infinite Loop" },
-    { key: "field4", value: "Cupertino, California, US" },
-    { key: "field5", value: "90210" },
-    { key: "field6", value: "Metaware Labs" },
-    { key: "field7", value: "www.metawarelabs.com" },
-    { key: "field8", value: "123 Yonge Street" },
-    { key: "field9", value: "Toronto, ON, Canada" },
-    { key: "field10", value: "M5S 1B6" },
+  const customer_inputs = [
+    { key: "name", value: "Mr. John Doe" },
+    { key: "web_link", value: "John Doe Designs Inc." },
+    { key: "address1", value: "1 Infinite Loop" },
+    { key: "address2", value: "Cupertino, California, US" },
+    { key: "postal", value: "90210" },
+  ];
+  const company_inputs = [
+    { key: "name", value: "Metaware Labs" },
+    { key: "web_link", value: "www.metawarelabs.com" },
+    { key: "address1", value: "123 Yonge Street" },
+    { key: "address2", value: "Toronto, ON, Canada" },
+    { key: "postal", value: "M5S 1B6" },
   ];
 
   const currencies = [
-    { text: "GBP", value: "British Pound (£)" },
-    { text: "CAD", value: "Canadian Dollar ($)" },
-    { text: "EUR", value: "Euro (€)" },
-    { text: "INR", value: "Indian Rupee (₹)" },
-    { text: "NOK", value: "Norwegian Krone (kr)" },
-    { text: "USD", value: "US Dollar ($)" },
+    { value: "GBP", text: "British Pound (£)" },
+    { value: "CAD", text: "Canadian Dollar ($)" },
+    { value: "EUR", text: "Euro (€)" },
+    { value: "INR", text: "Indian Rupee (₹)" },
+    { value: "NOK", text: "Norwegian Krone (kr)" },
+    { value: "USD", text: "US Dollar ($)" },
   ];
 
   const header = [
@@ -74,9 +88,34 @@ function App() {
     <div className="App">
       <ThemeProvider theme={theme}>
         <Container>
-          <InvoiceForm inputs={inputs} currencies={currencies}></InvoiceForm>
+          <InvoiceHeader image={metaware_logo}></InvoiceHeader>
+
+          <hr />
+
+          <InvoiceForm
+            customer_inputs={customer_inputs}
+            company_inputs={company_inputs}
+            currencies={currencies}
+          ></InvoiceForm>
 
           <Table header={header} content={content}></Table>
+
+          <div className="form-action-buttons">
+            <ButtonTextPrimary>Reset</ButtonTextPrimary>
+            <ButtonTextPrimary>Turn On Print Mode</ButtonTextPrimary>
+          </div>
+
+          <InvoiceFooter>
+            <Hyperlink
+              href="https://www.linkedin.com/in/lucassantoli/"
+              noDecoration
+            >
+              Lucas Santana&nbsp;
+            </Hyperlink>
+            <span>Made with&nbsp;</span>
+            <span className="red-text">♥</span>
+            <span>&nbsp;in Aracaju.</span>
+          </InvoiceFooter>
         </Container>
       </ThemeProvider>
     </div>
