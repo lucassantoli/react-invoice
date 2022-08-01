1360027ab94939b9dce04062e5e5db478f787136
Lucas Santana <lucas.santana349@gmail.com>
2022-08-01 17:02:38 -0300

    Adding callback prop to icon button

--- src/components/atoms/Buttons/ButtonIcon/index.tsx
+++ src/components/atoms/Buttons/ButtonIcon/index.tsx
@@ -5,17 +5,27 @@ import { ButtonIconPrimary, ButtonIconDanger } from "./styles";
 import Icon from "@mdi/react";
 import { mdiPlusThick, mdiCloseThick } from "@mdi/js";
 
-const ButtonAdd: FC = () => {
+interface Props {
+  callback?: Function;
+}
+
+const ButtonAdd: FC<Props> = ({ callback }) => {
   return (
-    <ButtonIconPrimary title="Add">
+    <ButtonIconPrimary
+      title="Add"
+      onClick={(e) => (callback ? callback(e) : null)}
+    >
       <Icon path={mdiPlusThick} size={1} color="white" />
     </ButtonIconPrimary>
   );
 };
 
-const ButtonDelete: FC = () => {
+const ButtonDelete: FC<Props> = ({ callback }) => {
   return (
-    <ButtonIconDanger title="Delete">
+    <ButtonIconDanger
+      title="Delete"
+      onClick={(e) => (callback ? callback(e) : null)}
+    >
       <Icon path={mdiCloseThick} size={1} color="white" />
     </ButtonIconDanger>
   );
